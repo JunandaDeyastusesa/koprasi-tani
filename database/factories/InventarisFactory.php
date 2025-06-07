bb5dd0ae1d04dd7c26b2a8165702816fc19bd381
azizah06 <azizahrosidah.com>
2025-06-07 21:44:05 +0700

    testing

--- database/factories/InventarisFactory.php
+++ database/factories/InventarisFactory.php
@@ -1,31 +1,32 @@
 <?php
-
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
+class InventarisFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'username' => $this->faker->unique()->userName(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+    public function definition()
+{
+    return [
+        'nama' => 'Item Test',
+        'kategori' => 'Test',
+        'harga_jual' => 100000,
+        'harga_beli' => 80000,
+        'jumlah' => 10,
+        'deskripsi' => 'Barang test',
+    ];
+}
+
 
     /**
      * Indicate that the model's email address should be unverified.
